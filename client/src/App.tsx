41eba73a0e62e213ba8df55ef8b7956a1940ab79
YaokunLin <yaokun1@ualberta.ca>
2023-03-22 10:42:44 -0700

    1st success api call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import graphql from 'babel-plugin-relay/macro';
+import { useLazyLoadQuery } from 'react-relay';
+
+const AppQuery = graphql`
+query AppQuery {
+  allTodos {
+    edges {
+      node {
+        completed
+        dateCreated
+        dateUpdated
+        userUid
+      }
+    }
+  }
+}
+`;
 
 function App() {
+  const data = useLazyLoadQuery(
+    AppQuery,
+    {},
+  );
+  console.log(data)
   return (
     <div className="App">
       hello world
